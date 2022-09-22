ab51ae428fdb8b29b2a48a1d380bb6bdad546a69
Ratteit, Friederike <friederike.ratteit@student.hs-anhalt.de>
2022-09-22 22:15:46 +0200

    grundlage angefangen

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import { Route, Link, Routes } from "react-router-dom";
+import { About, Movies, Help } from './content';
+ 
+class App extends Component {
+  render() {
+    return (
+        <div>
+          <h1>Simple SPA</h1>
+          <ul className="header">
+            <li><Link to="/">About</Link></li>
+            <li><Link to="/movies">Movies</Link></li>
+            <li><Link to="/help">Help</Link></li>
+          </ul>
+          <div className="content">
+            <Routes>
+              <Route exact path="/" element={<About/>}/>
+              <Route path="/movies" element={<Movies/>}/>
+              <Route path="/help" element={<Help/>}/>
+            </Routes>
+             
+          </div>
+        </div>
+    );
+  }
+}
+ 
+export default App;
---
+++ src/content/index.js
@@ -0,0 +1,3 @@
+export { default as About } from './about/About';
+export { default as Help } from './help/Help';
+export { default as Movies } from './movies/Movies';
